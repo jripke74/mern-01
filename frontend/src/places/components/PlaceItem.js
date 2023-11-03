edd9f7688b9385fc60ef0080960a823a98660a12
Jeff Ripke <jripke74@gmail.com>
2023-11-03 08:55:12 -0700

    add route params

--- frontend/src/places/components/PlaceItem.js
+++ frontend/src/places/components/PlaceItem.js
@@ -9,7 +9,7 @@ const PlaceItem = (props) => {
           <img src={props.image} alt={props.title} />
         </div>
         <div className="place-item__info">
-          <h2>{props.address}</h2>
+          <h2>{props.title}</h2>
           <h3>{props.address}</h3>
           <p>{props.description}</p>
         </div>
