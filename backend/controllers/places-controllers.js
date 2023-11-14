ad003b2d2ec16a758d7f849608089f646667af59
Jeff Ripke <jripke74@gmail.com>
2023-11-14 08:25:40 -0800

    add post route for createPlace

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -1,3 +1,5 @@
+const { v4: uuid } = require("uuid");
+
 const HttpError = require("../models/http-error");
 
 const DUMMY_PLACES = [
@@ -36,7 +38,7 @@ const getPlaceByUserId = (req, res, next) => {
   });
 
   if (!place) {
-    return new next(
+    return next(
       new HttpError("Could nont find a place with the provided user id.", 404)
     );
   }
@@ -44,5 +46,22 @@ const getPlaceByUserId = (req, res, next) => {
   res.json({ place });
 };
 
+const createPlace = (req, res, next) => {
+  const { title, description, coordinates, address, creator } = req.body;
+  const createdPlace = {
+    id: uuid(),
+    title,
+    description,
+    location: coordinates,
+    address,
+    creator,
+  };
+
+  DUMMY_PLACES.push(createdPlace);
+
+  res.status(201).json({ place: createdPlace });
+};
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
+exports.createPlace = createPlace;
