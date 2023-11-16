ca64c7a376e91426bad389804fc9c46220588795
Jeff Ripke <jripke74@gmail.com>
2023-11-16 14:24:16 -0800

    connect places and users

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -35,7 +35,7 @@ const signup = async (req, res, next) => {
       new HttpError("Invalid inputs passed, please check your data.", 422)
     );
   }
-  const { name, email, password, places } = req.body;
+  const { name, email, password } = req.body;
 
   let existingUser;
   try {
@@ -61,7 +61,7 @@ const signup = async (req, res, next) => {
     email,
     password,
     image: "kdsg",
-    places,
+    places: [],
   });
 
   try {
