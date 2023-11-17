7726aebf6921c97c71999fa57742241d883c7d3a
Jeff Ripke <jripke74@gmail.com>
2023-11-17 11:13:54 -0800

    change db name to mern

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    `mongodb+srv://${mongoDbUsername}:${mongoDbPassword}@cluster0.mxuwfw1.mongodb.net/places?retryWrites=true&w=majority`
+    `mongodb+srv://${mongoDbUsername}:${mongoDbPassword}@cluster0.mxuwfw1.mongodb.net/mern?retryWrites=true&w=majority`
   )
   .then(() => {
     app.listen(5003);
